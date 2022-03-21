e18e0a3c59623e5ee7708a448e8d64c47e8367ac
Italo Melo <italosm29@gmail.com>
2022-03-21 19:09:08 -0300

    first commit

---
+++ src/templates/Home/index.tsx
@@ -0,0 +1,10 @@
+import { CloneMe } from '../../components/CloneMe';
+import * as Styled from './styles';
+
+export function Home() {
+  return (
+    <Styled.Wrapper>
+      <CloneMe title="I am a clone?" />
+    </Styled.Wrapper>
+  );
+}
